5aca79bf231094f4620ae9a4bdbe8da955881129
naumanaziz <aziznauman96@hotmail.it>
2018-04-07 14:28:46 +0200

    interface

--- Context/Models/ContextM.cs
+++ Context/Models/ContextM.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContextM{
+	public interface IContext{ 
+		bool Partecipa(int idU, int idC);
+		void Vota(int idU, int idP);
+		bool Iscriviti(Utente utente);
+		List<Sfida> ViewSfide(int idC);
+		Sfida ViewSfide(int idC);
+		void AddSfida(Sfida sfida);
+		bool FineSfida(Sfida sfida);
+	}
+
+	public class ImplContext: IContext {
+
+	}
+
+}
