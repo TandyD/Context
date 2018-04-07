ac13f4a91281df67b28aa670a7cbe5582ec53ec9
TandyD <brinzila30@gmail.com>
2018-04-07 14:54:29 +0200

    InitContext

---
+++ DataBase/CreateDB.sql
@@ -0,0 +1,23 @@
+﻿create database ContextDB;
+use ContextDB;
+
+create table Utenti(
+	id int identity(1,1) not null primary key,
+	username nvarchar (20)not null unique,
+	passwd nvarchar (30)not null,
+	nome varchar (20) not null,
+	cognome varchar (20) not null,
+	dataNascita date,
+);
+
+create table Partecipanti(
+	id int identity(1,1) not null primary key,
+	idUt int foreign key references Utenti,
+	idContext int foreign key references Context
+);
+
+create table Context (
+	
+);
+
+
